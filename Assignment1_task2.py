e116cb2042c0af2223bb45e384c10ad1d2de78c9
Yogesh Puri <puriyogesh771@gmail.com>
2025-10-05 12:54:43 +0530

    Add files via upload

---
+++ Assignment1_task2.py
@@ -0,0 +1,11 @@
+# Task 2: Create a Personalized Greeting
+
+# Taking first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Combining names
+full_name = first_name + " " + last_name
+
+# Printing a personalized greeting
+print(f"\nHello, {full_name}! Welcome to the Python program.")
